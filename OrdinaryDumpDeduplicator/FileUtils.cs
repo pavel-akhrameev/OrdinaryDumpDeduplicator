5b134b889615f7659579cf53f3382d2a930c4650
Pavel Akhrameev <pavel.akhrameev@outlook.com>
2019-09-18 23:03:47 +0500

    temp

---
+++ OrdinaryDumpDeduplicator/FileUtils.cs
@@ -0,0 +1,16 @@
+﻿using OrdinaryDumpDeduplicator.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdinaryDumpDeduplicator
+{
+    public static class FileUtils
+    {
+
+        public File MakeFileInfo(String filePath)
+        {
+
+        }
+    }
+}
