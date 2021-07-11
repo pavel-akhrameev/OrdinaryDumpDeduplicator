8a1fe0846602ed93f600a3d1b2220a3c1119b7b1
Pavel Akhrameev <pavel.akhrameev@outlook.com>
2021-07-11 16:00:00 +0400

    OrdinaryDumpDeduplicator added.

---
+++ OrdinaryDumpDeduplicator/OrdinaryDumpDeduplicator.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using OrdinaryDumpDeduplicator.Common;
+
+namespace OrdinaryDumpDeduplicator
+{
+    public class OrdinaryDumpDeduplicator
+    {
+        private readonly DuplicatesProcessor _duplicatesProcessor;
+        private readonly DataControllerSimple _dataController;
+
+        private DataLocation _currentDataLocation;
+
+        #region Constructor and initializer
+
+        public OrdinaryDumpDeduplicator()
+        {
+            this._dataController = new DataControllerSimple();
+            this._duplicatesProcessor = new DuplicatesProcessor(_dataController);
+            this._currentDataLocation = null;
+        }
+
+        public void Initialize()
+        {
+            _dataController.Initialize();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public DataLocation AddDataLocation(String directoryPath)
+        {
+            // TODO: check path provided.
+
+            var directoryInfo = new System.IO.DirectoryInfo(directoryPath);
+            var directory = new Directory(directoryInfo.Name, parentDirectory: null, directoryPath);
+            _dataController.AddDirectory(directory);
+
+            DataLocation dataLocation = GetDataLocation(directory);
+            return dataLocation;
+        }
+
+        public DataLocation DoInspection(DataLocation dataLocation)
+        {
+            _currentDataLocation = null;
+            var inspection = new Inspection(dataLocation, DateTime.Now);
+            _dataController.AddInspection(inspection);
+
+            FsUtils.IndexSubDirectoriesAndFiles(
+               dataLocation.Path,
+               out Directory rootDirectory,
+               out IReadOnlyCollection<Directory> fsFolders,
+               out IReadOnlyCollection<File> fsFiles);
+
+            _dataController.AddDirectory(rootDirectory); // Тут просто сохраняем структуру папок-файлов в БД.
+
+            // Собрать атрибуты всех файлов, включая размер. Сохранить FileState в статусе Unknown или Error (если не получилось доcтать атрибуты).
+            IReadOnlyCollection<FileState> statesOfFiles = GetAttributesOfFiles(fsFiles, inspection);
+
+            CountFilesAndSize(statesOfFiles, out Int32 filesCount, out Int64 dataSize);
+
+            // Получить и сохранить хеши всех файлов. Обновить FileState до New.
+            ComputeHashesOfFiles(statesOfFiles);
+
+            inspection.FinishInspection(DateTime.Now);
+            _dataController.UpdateInspection(inspection);
+
+            _currentDataLocation = dataLocation;
+            return _currentDataLocation;
+        }
+
+        public void ComputeHashesOfFiles(IReadOnlyCollection<FileState> statesOfFiles)
+        {
+            foreach (FileState fileState in statesOfFiles)
+            {
+                FileStatus fileStatus;
+                BlobInfo blobInfo;
+
+                try
+                {
+                    blobInfo = ComputeAndSaveBlobInfo(fileState.File.Path);
+                    fileStatus = FileStatus.New;
+                }
+                catch (Exception ex)
+                {
+                    blobInfo = BlobInfo.BrokenBlobInfo;
+                    fileStatus = FileStatus.Unreadable;
+                }
+
+                fileState.SetStatusAndBlobInfo(fileStatus, blobInfo);
+                _dataController.UpdateFileState(fileState);
+            }
+        }
+
+        public BlobInfo ComputeAndSaveBlobInfo(String path)
+        {
+            BlobInfo blobInfo;
+
+            try
+            {
+                Byte[] sha1HashSum = FsUtils.ComputeSha1Hash(path, out Int64 fileLength);
+                blobInfo = BlobInfo.Create(fileLength, sha1HashSum);
+            }
+            catch (Exception ex)
+            {
+                blobInfo = BlobInfo.BrokenBlobInfo;
+            }
+
+            _dataController.AddBlobInfo(blobInfo);
+            return blobInfo;
+        }
+
+        public DuplicateReport GetDuplicatesFound(IReadOnlyCollection<DataLocation> dataLocations)
+        {
+            DuplicateReport duplicateReport = _duplicatesProcessor.GetDuplicatesFound(dataLocations);
+            return duplicateReport;
+        }
+
+        public void MoveKnownDuplicatesToSpecialFolder(DuplicateReport duplicateReport, HierarchicalObject[] hierarchicalObjects)
+        {
+            _duplicatesProcessor.MoveKnownDuplicatesToSpecialFolder(duplicateReport, hierarchicalObjects);
+        }
+
+        public void DeleteDuplicate(DuplicateReport duplicateReport, HierarchicalObject hierarchicalObject)
+        {
+            _duplicatesProcessor.DeleteDuplicate(duplicateReport, hierarchicalObject);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private IReadOnlyCollection<FileState> GetAttributesOfFiles(IReadOnlyCollection<File> files, Inspection inspection)
+        {
+            var result = new List<FileState>(files.Count);
+
+            foreach (File file in files)
+            {
+                FileState fileState;
+
+                try
+                {
+                    var fileInfo = new System.IO.FileInfo(file.Path);
+
+                    var status = FileStatus.Unknown;
+                    fileState = new FileState(file, inspection, previousState: null, fileInfo.Length, status, fileInfo.CreationTime, fileInfo.LastWriteTime);
+                }
+                catch (Exception ex)
+                {
+                    // TODO: log exception.
+
+                    var blobInfo = BlobInfo.BrokenBlobInfo;
+                    var status = FileStatus.Error;
+                    var currentDateTime = DateTime.Now;
+                    fileState = new FileState(file, inspection, previousState: null, size: -1, status, creationDate: currentDateTime, modificationDate: currentDateTime, blobInfo);
+                }
+
+                _dataController.AddFileState(fileState);
+                result.Add(fileState);
+            }
+
+            return result;
+        }
+
+        private DataLocation GetDataLocation(Directory directory)
+        {
+            DataLocation dataLocation;
+            IReadOnlyCollection<DataLocation> dataLocations = _dataController.GetDataLocations(new[] { directory });
+            if (dataLocations.Count > 0)
+            {
+                dataLocation = dataLocations.First();
+            }
+            else
+            {
+                dataLocation = new DataLocation(directory);
+                _dataController.AddDataLocation(dataLocation);
+            }
+
+            return dataLocation;
+        }
+
+        /// <remarks>Counts only available files.</remarks>
+        private void CountFilesAndSize(IReadOnlyCollection<FileState> statesOfFiles, out Int32 filesCount, out Int64 dataSize)
+        {
+            filesCount = 0;
+            dataSize = 0;
+            foreach (FileState fileState in statesOfFiles)
+            {
+                if (fileState.Status != FileStatus.Error &&
+                    fileState.Status != FileStatus.Unreadable &&
+                    fileState.Size > 0)
+                {
+                    filesCount++;
+                    dataSize += fileState.Size;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
